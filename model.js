2d6e1317c9d1dca825b2467aa908743b0b5471ab
Superjim <jamesjenkinson10@gmail.com>
2023-01-16 16:04:06 +0000

    Completed api/articles

--- model.js
+++ model.js
@@ -17,8 +17,8 @@ const fetchAllArticles = () => {
   return database
     .query(
       `
-        SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) as comment_count
-        FROM articles
+        SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) as comment_count 
+        FROM articles 
         JOIN comments 
         ON articles.article_id = comments.article_id 
         GROUP BY articles.article_id, comments.article_id, articles.author 
