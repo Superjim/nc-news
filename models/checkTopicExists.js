d24e11150a879faffdb5720c476446b9fcb15124
Superjim <jamesjenkinson10@gmail.com>
2023-01-22 15:59:14 +0000

    completed add new topic

--- models/checkTopicExists.js
+++ models/checkTopicExists.js
@@ -1,17 +1,22 @@
 const { fetchAllTopics } = require("./fetchAllTopics");
 
-//This function retrieves a list of topics from the database. You can whitelist as many additional valid topics as you want. A use case of this would be to pass in "" which will default to all topics
-const checkTopicExists = async (topic, ...additionalTopics) => {
+//This function retrieves a list of topics from the database.
+const checkTopicExists = async (topic, topicShouldExist = true) => {
   //fetch array of all topics on database
   const topics = await fetchAllTopics();
   //create new array of the topic slug (topic name)
   validTopic = topics.map((topic) => topic.slug);
-  //push optional additional topics
-  validTopic = validTopic.concat(additionalTopics);
-  if (!validTopic.includes(topic)) {
+  //If the topic does not exist and topicShouldExist is (default) true, the function returns a rejected promise
+  if (!validTopic.includes(topic) && topicShouldExist === true) {
     return Promise.reject({
       status: 404,
       msg: `Topic ${topic} does not exist`,
+    });
+    //If the topic does exist and topicShouldExist is false, the function returns a rejected promise
+  } else if (validTopic.includes(topic) && topicShouldExist === false) {
+    return Promise.reject({
+      status: 400,
+      msg: `Invalid request: topic ${topic} already exists`,
     });
   } else {
     return true;
