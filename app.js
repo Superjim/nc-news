84b4c5d1c61eaf06cb917979e0c7e44728f68454
Superjim <jamesjenkinson10@gmail.com>
2023-02-06 10:25:17 +0000

    allowed cors

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const cors = require("cors");
-app.use(cors());
 const { response } = require("express");
 const express = require("express");
 const app = express();
+app.use(cors());
 
 const {
   getAllTopics,
